dbbd59a201afaa56a8a5fd6768b2a2fbcd5d13ea
Kingali78945 <78742869+Kingali78945@users.noreply.github.com>
2021-08-17 13:26:28 +0100

    Update students.php

--- students.php
+++ students.php
@@ -8,3 +8,13 @@ if (isset($_SESSION['id'])) {
 
    echo template("templates/partials/header.php");
    echo template("templates/partials/nav.php");
+
+$sql = "SELECT studentid,dob,firstname,lastname,house,town,county,country,postcode FROM student;";
+
+   $result = mysqli_query($conn,$sql);
+
+   $data['content'] .= '<form method="post">';
+   $data['content'] .= "<table border='1'>";
+   $data['content'] .= "<h2>Students</h2><table class='table table-hover'>";
+   $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th>
+   <th>County</th><th>Country</th><th>Postcode</th><th>Select</th></tr>";
