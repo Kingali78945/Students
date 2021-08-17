8feec69b01462e6e4704fd6a9f3a17c6a793d5a9
Kingali78945 <78742869+Kingali78945@users.noreply.github.com>
2021-08-17 13:25:50 +0100

    Update students.php

---
+++ students.php
@@ -0,0 +1,10 @@
+<?php
+include("_includes/config.inc");
+include("_includes/dbconnect.inc");
+include("_includes/functions.inc");
+
+// check logged in
+if (isset($_SESSION['id'])) {
+
+   echo template("templates/partials/header.php");
+   echo template("templates/partials/nav.php");
