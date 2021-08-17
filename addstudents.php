6604b30f1cea521140e92a624b9ca8cc19f59352
Kingali78945 <78742869+Kingali78945@users.noreply.github.com>
2021-08-17 14:19:45 +0100

    Update addstudents.php

--- addstudents.php
+++ addstudents.php
@@ -51,3 +51,14 @@ Student Image:
 
 </form>
 </html>
+
+<?php
+   include("_includes/config.inc");
+   include("_includes/dbconnect.inc");
+   include("_includes/functions.inc");
+   // check logged in
+   if (isset($_SESSION['id'])) {
+     echo template("templates/partials/header.php");
+     echo template("templates/partials/nav.php");
+     
+$data['content'] .= '</form>';
