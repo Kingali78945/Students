cfaded2e57800ffe04a66efd704761cb32809d4c
Kingali78945 <78742869+Kingali78945@users.noreply.github.com>
2021-08-17 13:27:41 +0100

    Update students.php

--- students.php
+++ students.php
@@ -18,3 +18,35 @@ $sql = "SELECT studentid,dob,firstname,lastname,house,town,county,country,postco
    $data['content'] .= "<h2>Students</h2><table class='table table-hover'>";
    $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th>
    <th>County</th><th>Country</th><th>Postcode</th><th>Select</th></tr>";
+   
+    while($row = mysqli_fetch_array($result)) 
+   
+   {
+      $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[dob] </td>";
+      $data['content'] .= "<td> $row[firstname] </td><td> $row[lastname] </td>
+      <td> $row[house] </td><td> $row[town] </td><td> $row[county] </td><td> $row[country] </td>
+      <td> $row[postcode] </td> </td> ";
+      $data['content'] .= "<td> <input type ='checkbox' name='delrecords[]' value='".$row['studentid']."' </td></tr> </td>";
+   }
+
+   $data['content'] .= "</table>";
+   $data['content'] .= "</br></br></br>";
+   $data['content'] .= '<input type="submit" name="delete" value="Delete Records">';
+   $data['content'] .= "</form>";
+
+
+   if(isset($_POST['delete']))
+   
+   {
+     $checkboxcount = count($_POST['delrecords']);
+     $i=0;
+     while($i<$checkboxcount)
+     {                                 //count of records to delete
+       $theid = $_POST['delrecords'][$i];
+       mysqli_query($conn, "DELETE FROM student WHERE studentid= '$theid'");
+       $i++;
+
+     }
+
+      echo "<H3>Success: Data successfully removed!</H3>";
+   }
