3d65d80d51fc3d1e0984ef91ea041e71c825e246
Kingali78945 <78742869+Kingali78945@users.noreply.github.com>
2021-08-18 22:17:33 +0100

    update

--- students.php
+++ students.php
@@ -34,21 +34,6 @@ $sql = "SELECT studentid,dob,firstname,lastname,house,town,county,country,postco
    $data['content'] .= "</form>";
 
 
-   if(isset($_POST['delete']))
-   
-   {
-     $checkboxcount = count($_POST['delrecords']);
-     $i=0;
-     while($i<$checkboxcount)
-     {                             
-       $theid = $_POST['delrecords'][$i];
-       mysqli_query($conn, "DELETE FROM student WHERE studentid= '$theid'");
-       $i++;
-
-     }
-
-      echo "<H3>Success: Data successfully removed!</H3>";
-   }
   echo template("templates/default.php", $data);
 } 
 else 
