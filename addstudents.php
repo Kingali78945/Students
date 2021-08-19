28fd4c1e0600a0689775682b1523b91812b17af5
Kingali78945 <78742869+Kingali78945@users.noreply.github.com>
2021-08-19 14:04:18 +0100

    changes

--- addstudents.php
+++ addstudents.php
@@ -1,8 +1,6 @@
 <html>
 <h2> Detail Of Student</h2>
-
 <form enctype ="multipart/form-data" action ="savestudents.php" method ="post">
-
 Student ID:
 <input type ="number" name ="numid" class ="form-control" />
 DOB:
