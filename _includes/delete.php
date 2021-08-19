c54a84b47d7b09c7cf70ae5b0f120739d5639064
Kingali78945 <78742869+Kingali78945@users.noreply.github.com>
2021-08-19 13:31:43 +0100

    Update delete.php

--- _includes/delete.php
+++ _includes/delete.php
@@ -2,24 +2,21 @@
 include_once("config.inc");
 include_once("dbconnect.inc");
 include_once("functions.inc");
-// check logged in
 if (isset($_SESSION['id'])) {
-
    echo template("templates/partials/header.php");
    echo template("templates/partials/nav.php");
 }
-	$students = $_POST['StudentID'];
+	$student = $_POST['StudentID'];
 	
-	if (empty($students)) {
+	if (empty($student)) {
 		echo 'no student selected';
 	}
 	else {
-		foreach ($students as $studentid) {
+		foreach ($student as $studentid) {
 			$sql = "DELETE from student WHERE studentid='$studentid';";
-			//echo $sql;
 			mysqli_query($conn,$sql);
 			
 		}
 	}
 	
-	header("Location: ../students.php");
+	header("Location: ../student.php");
