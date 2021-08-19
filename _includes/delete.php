81f88ddf49272e2752964b584bae38a079e1e6a6
Kingali78945 <78742869+Kingali78945@users.noreply.github.com>
2021-08-19 13:48:24 +0100

    Update delete.php

--- _includes/delete.php
+++ _includes/delete.php
@@ -2,21 +2,20 @@
 include_once("config.inc");
 include_once("dbconnect.inc");
 include_once("functions.inc");
-if (isset($_SESSION['id'])) {
-   echo template("templates/partials/header.php");
-   echo template("templates/partials/nav.php");
-}
-	$student = $_POST['StudentID'];
-	
-	if (empty($student)) {
-		echo 'no student selected';
-	}
-	else {
-		foreach ($student as $studentid) {
-			$sql = "DELETE from student WHERE studentid='$studentid';";
-			mysqli_query($conn,$sql);
-			
-		}
-	}
+if(isset($_POST['delete']))
+
+   {
+     $checkboxcount = count($_POST['delrecords']);
+     $i=0;
+     while($i<$checkboxcount)
+     {                             
+       $theid = $_POST['delrecords'][$i];
+       mysqli_query($conn, "DELETE FROM student WHERE studentid= '$theid'");
+       $i++;
+
+     }
+
+      echo "<H3>Success: Data successfully removed!</H3>";
+   }
 	
 	header("Location: ../student.php");
