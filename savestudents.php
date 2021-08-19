7616b40e884a14958fa02b21640cfd310e8ad5d9
Kingali78945 <78742869+Kingali78945@users.noreply.github.com>
2021-08-19 12:05:41 +0100

    Update savestudents.php

--- savestudents.php
+++ savestudents.php
@@ -9,6 +9,7 @@ $sql = "INSERT INTO student";
 $sql = "(StudentID, Password, DOB, FirstName, LastName, House, Town, County, Country, PostCode, Image)";
 $sql = "VALUES ('$_POST[numid]', '$_POST[txtpassword]', $_POST[dob]', '$_POST[txtfirstname]', '$_POST[txtlastname]', '$_POST[txthouse]', '$_POST[txttown]', '$_POST[txtcounty]', '$_POST[txtcountry]', '$_POST[txtpostcode]', '$imagedata');";
 
+
 $result = mysqli_query($db, $sql);
 
 echo $sql;
