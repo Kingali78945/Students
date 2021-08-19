b598fbecdf4668ebc8171a2468e9b03be720cc96
Kingali78945 <78742869+Kingali78945@users.noreply.github.com>
2021-08-19 13:41:21 +0100

    Update students.php

--- students.php
+++ students.php
@@ -4,14 +4,11 @@ include("_includes/dbconnect.inc");
 include("_includes/functions.inc");
 
 if (isset($_SESSION['id'])) {
-
    echo template("templates/partials/header.php");
    echo template("templates/partials/nav.php");
-
 $sql = "SELECT studentid,dob,firstname,lastname,house,town,county,country,postcode FROM student;";
 
    $result = mysqli_query($conn,$sql);
-
    $data['content'] .= '<form method="post">';
    $data['content'] .= "<table border='1'>";
    $data['content'] .= "<h2>Students</h2><table class='table table-hover'>";
@@ -19,7 +16,6 @@ $sql = "SELECT studentid,dob,firstname,lastname,house,town,county,country,postco
    <th>County</th><th>Country</th><th>Postcode</th><th>Select</th></tr>";
    
    while($row = mysqli_fetch_array($result)) 
-   
    {
       $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[dob] </td>";
       $data['content'] .= "<td> $row[firstname] </td><td> $row[lastname] </td>
@@ -27,11 +23,27 @@ $sql = "SELECT studentid,dob,firstname,lastname,house,town,county,country,postco
       <td> $row[postcode] </td> </td> ";
       $data['content'] .= "<td> <input type ='checkbox' name='delrecords[]' value='".$row['studentid']."' </td></tr> </td>";
    }
-
    $data['content'] .= "</table>";
    $data['content'] .= "</br></br></br>";
    $data['content'] .= '<input type="submit" name="delete" value="Delete Records">';
    $data['content'] .= "</form>";
+   
+   if(isset($_POST['delete']))
+
+   {
+     $checkboxcount = count($_POST['delrecords']);
+     $i=0;
+     while($i<$checkboxcount)
+     {                             
+       $theid = $_POST['delrecords'][$i];
+       mysqli_query($conn, "DELETE FROM student WHERE studentid= '$theid'");
+       $i++;
+
+     }
+
+      echo "<H3>Success: Data successfully removed!</H3>";
+   }
+
 
   echo template("templates/default.php", $data);
 } 
