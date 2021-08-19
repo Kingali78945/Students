b0dbbe0114844ff2ef7924bae89fa4dbd6b03c1c
Kingali78945 <78742869+Kingali78945@users.noreply.github.com>
2021-08-19 13:07:11 +0100

    Update savestudents.php

--- savestudents.php
+++ savestudents.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 $db = mysqli_connect("localhost", "root", "", "students"
 $image = $_FILES['studentimage']['tmp_name'];
-$sql = "INSERT INTO student (StudentID, Password, DOB, FirstName, LastName, House, Town, County, Country, PostCode, Image) VALUES ('$_POST[numid]', '$_POST[txtpassword]', $_POST[dob]', '$_POST[txtfirstname]', '$_POST[txtlastname]', '$_POST[txthouse]', '$_POST[txttown]', '$_POST[txtcounty]', '$_POST[txtcountry]', '$_POST[txtpostcode]', '$imagedata')
+$sql = "INSERT INTO student (StudentID, Password, DOB, FirstName, LastName, House, Town, County, Country, PostCode, Image) VALUES ('$_POST[numid]', '$_POST[txtpassword]', $_POST[dob]', '$_POST[txtfirstname]', '$_POST[txtlastname]', '$_POST[txthouse]', '$_POST[txttown]', '$_POST[txtcounty]', '$_POST[txtcountry]', '$_POST[txtpostcode]', '$imagedata');");
 $result = mysqli_query($db, $sql);
 echo $sql;
 mysqli_close($db);
