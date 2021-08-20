a4b09cb118aa36a804a710ddd53339ca64518d07
Kingali78945 <78742869+Kingali78945@users.noreply.github.com>
2021-08-20 13:58:12 +0100

    Update students.php

--- students.php
+++ students.php
@@ -9,9 +9,9 @@ if (isset($_SESSION['id'])) {
    $sql = "select * from student;";
    $result = $conn->query($sql);
  while($row = mysqli_fetch_assoc($result)) {
-      $data['content'] .= "form method="post"><h2>Students</h2><table class='table table-hover'>";
+      $data['content'] .= '<form method="post"><h2>Students</h2><table class='table table-hover'>';
       $data['content'] .= "<thead>";
-      $data['content'] .= "<tr><th>FirstName</th><th>Lastname</th><th>dob</th><th>House</th><th>Town</th><th>County</th><th>Country</th><th>Postcode</th><th>select</th></tr></thead><tbody>";
+      $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th><th>County</th><th>Country</th><th>Postcode</th><th>Select</th></tr>";
       while($row = mysqli_fetch_array($result)) {
          $data['content'] .= "<td> $row[firstname] </td><td> $row[lastname] </td>";
          $data['content'] .= "<td> $row[dob] </td><td> $row[house]</td><td> $row[town]</td><td> $row[county] </td> <td> $row[country] </td><td> $row[postcode] </td><td><input type='checkbox' name='StudentID[]'  value='$row[studentid]'></td></tr>";
