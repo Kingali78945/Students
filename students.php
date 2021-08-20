d82b1e693e7aac9d24d12a6bb559ed2658a57ac0
Kingali78945 <78742869+Kingali78945@users.noreply.github.com>
2021-08-20 14:02:40 +0100

    Update students.php
    I was trying out another method too see if it worked but sadly it didn't so using my original one

--- students.php
+++ students.php
@@ -3,21 +3,28 @@ include("_includes/config.inc");
 include("_includes/dbconnect.inc");
 include("_includes/functions.inc");
 
-if (isset($_SESSION['id'])) {
+ if (isset($_SESSION['id'])) {
    echo template("templates/partials/header.php");
    echo template("templates/partials/nav.php");
    $sql = "select * from student;";
    $result = $conn->query($sql);
- while($row = mysqli_fetch_assoc($result)) {
-      $data['content'] .= '<form method="post"><h2>Students</h2><table class='table table-hover'>';
-      $data['content'] .= "<thead>";
-      $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th><th>County</th><th>Country</th><th>Postcode</th><th>Select</th></tr>";
-      while($row = mysqli_fetch_array($result)) {
-         $data['content'] .= "<td> $row[firstname] </td><td> $row[lastname] </td>";
-         $data['content'] .= "<td> $row[dob] </td><td> $row[house]</td><td> $row[town]</td><td> $row[county] </td> <td> $row[country] </td><td> $row[postcode] </td><td><input type='checkbox' name='StudentID[]'  value='$row[studentid]'></td></tr>";
-      }
-      $data['content'] .= "</tbody></table>  <div><input type='submit' name='Delete' value='Delete'></div></form>";
-       }
+   $data['content'] .= '<form method="post">';
+   $data['content'] .= "<table border='1'>";
+   $data['content'] .= "<h2>Students</h2><table class='table table-hover'>";
+   $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th>
+   <th>County</th><th>Country</th><th>Postcode</th><th>Select</th></tr>";
+   while($row = mysqli_fetch_array($result)) 
+   {
+      $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[dob] </td>";
+      $data['content'] .= "<td> $row[firstname] </td><td> $row[lastname] </td>
+      <td> $row[house] </td><td> $row[town] </td><td> $row[county] </td><td> $row[country] </td>
+      <td> $row[postcode] </td> </td> ";
+      $data['content'] .= "<td> <input type ='checkbox' name='delrecords[]' value='".$row['studentid']."' </td></tr> </td>";
+   }
+   $data['content'] .= "</table>";
+   $data['content'] .= "</br></br></br>";
+   $data['content'] .= '<input type="submit" name="delete" value="Delete Records">';
+   $data['content'] .= "</form>";
 	   if(isset($_POST['delete']))
    {
      $checkboxcount = count($_POST['delrecords']);
